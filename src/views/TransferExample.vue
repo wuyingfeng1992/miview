7a03b6fc7c0adbb6793e3b3cb22562c102316be5
ly2011 <1518550424@qq.com>
2018-11-18 21:59:41 +0800

    feat: 完善transferExample的用法

--- src/views/TransferExample.vue
+++ src/views/TransferExample.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <ly-transfer v-model="value1" :data="data" />
+    <ly-transfer v-model="value1" :data="data" filterable filter-placeholder="请输入备选项名称" :left-default-checked="[2, 3]" :right-default-checked="[1]" :titles="['来源', '目标']" :button-texts="['到左边', '到右边']" :format="{
+        noChecked: '${total}',
+        hasChecked: '${checked}/${total}'
+      }">
+      <span slot-scope="{ option }">{{ option.key }} - {{ option.label }}</span>
+      <el-button class="transfer-footer" slot="left-footer" size="small">操作</el-button>
+      <el-button class="transfer-footer" slot="right-footer" size="small">操作</el-button>
+    </ly-transfer>
   </div>
 </template>
 <script>
