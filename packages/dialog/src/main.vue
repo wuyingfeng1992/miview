693425347fb75b30917a27195a5d38454081dc57
ly2011 <1518550424@qq.com>
2018-11-26 17:26:14 +0800

    feat: 添加 弹框组件

---
+++ packages/dialog/src/main.vue
@@ -0,0 +1,100 @@
+<template>
+  <el-dialog ref="elDialog" :visible.sync="isShow" v-if="!destroyedWhenClose || isShow" :width="setWidth" :class="['i-dialog', customClass, modeClass]" @close="handleDialogClose" :close-on-click-modal="closeOnClickModal" :close-on-press-escape="closeOnPressEscape" :before-close="closeBefore" :title="title" :show-close="showClose" :modal="getModal" top="0" :append-to-body="appendToBody">
+    <div class="customBody">
+      <slot></slot>
+    </div>
+    <div v-if="toolbar && toolbar.length" slot="footer" class="dialog-footer">
+      <el-button v-for="(btn, idx) of toolbar" v-show="!btn.hide" :disabled="btn.disabled" :loading="btn.loading" :key="idx" :type="btn.type || 'primary'" @click="handleClick(btn)">
+        {{btn.text}}
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'iDialog',
+  props: {
+    mode: {
+      type: String,
+      default: 'standard'
+    },
+    width: String,
+    appendToBody: {
+      type: Boolean,
+      default: true
+    },
+    closeOnPressEscape: {
+      type: Boolean,
+      default: true
+    },
+    closeOnClickModal: {
+      type: Boolean,
+      default: false
+    },
+    beforeClose: Function,
+    destroyedWhenClose: Boolean,
+    toolbar: Array,
+    modal: {
+      type: Boolean,
+      default: true
+    },
+    showClose: {
+      type: Boolean,
+      default: true
+    },
+    value: Boolean,
+    title: String,
+    size: { // 弹窗显示宽度 | String | mini/small/standard/large/huge | standard
+      type: String,
+      default: 'standard'
+    },
+    customClass: String
+  },
+  data () {
+    return {
+      isShow: this.value,
+      sizeMap: {
+        'huge': '1200px',
+        'large': '900px',
+        'standard': '700px',
+        'small': '500px',
+        'mini': '350px'
+      }
+    }
+  },
+  computed: {
+    modeClass () {
+      return this.mode === 'popup' && 'popup'
+    },
+    getModal () {
+      return this.mode === 'popup' ? false : this.modal
+    },
+    setWidth () {
+      let width = ''
+      if (this.mode === 'popup') {
+        width = '100%'
+      } else {
+        width = this.width || this.sizeMap[this.size]
+      }
+    }
+  },
+  methods: {
+    closeBefore (done) {
+      if (this.beforeClose) {
+        this.beforeClose(() => {
+          done()
+          this.$emit('input', false)
+        })
+      } else {
+        done()
+        this.$emit('input', false)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
