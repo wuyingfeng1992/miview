40036c9bd8f14b9dc7c4995ae10f43769ddd7383
ly2011 <1518550424@qq.com>
2019-01-22 09:25:41 +0800

    [release] 0.2.8

--- config/babelrc.js
+++ config/babelrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
   presets: [
     [
       '@babel/preset-env',
@@ -11,14 +11,48 @@ module.exports = {
       }
     ]
   ],
+  plugins: [[
+    '@babel/plugin-transform-runtime',
+    {
+      corejs: false,
+      helpers: true,
+      regenerator: false,
+      useESModules
+    }
+  ],'syntax-dynamic-import', 'transform-vue-jsx']
+}
+ */
+
+const { BABEL_MODULE, NODE_ENV } = process.env
+const useESModules = BABEL_MODULE !== 'commonjs' && NODE_ENV !== 'test'
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        modules: useESModules ? false : 'commonjs'
+      }
+    ],
+    [
+      '@vue/babel-preset-jsx',
+      {
+        functional: true
+      }
+    ]
+  ],
   plugins: [
-    'syntax-dynamic-import',
     [
       '@babel/plugin-transform-runtime',
       {
-        corejs: 2
+        corejs: false,
+        helpers: true,
+        regenerator: false,
+        useESModules
       }
     ],
-    'transform-vue-jsx'
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-object-assign'
   ]
 }
