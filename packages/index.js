212f941bb2d3d0a67cdfe35a2bc2025702ed591c
ly2011 <1518550424@qq.com>
2019-01-22 22:17:56 +0800

    [release] 0.2.10

--- packages/index.js
+++ packages/index.js
@@ -12,7 +12,7 @@ import Table from './table';
 import Toast from './toast';
 import Transfer from './transfer';
 import TreeSelect from './tree-select';
-const version = '0.2.9';
+const version = '0.2.10';
 const components = [
   BackToTop,
   Dialog,
