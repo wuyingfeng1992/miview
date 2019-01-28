adbb9cb0850497439ffdd8fa46ca891a421fecaa
ly2011 <1518550424@qq.com>
2019-01-28 11:28:51 +0800

    [release] 0.2.14

--- packages/index.js
+++ packages/index.js
@@ -15,7 +15,7 @@ import Table from './table';
 import Toast from './toast';
 import Transfer from './transfer';
 import TreeSelect from './tree-select';
-const version = '0.2.13';
+const version = '0.2.14';
 const components = [
   BackTop,
   Dialog,
